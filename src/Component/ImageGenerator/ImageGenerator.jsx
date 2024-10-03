5eedeff42ab6d472de9069fcf95b6c97a1fe2903
hamzarahmoun <hamza.rahmoun456@gmail.com>
2024-10-03 09:16:34 +0100

    header css

--- src/Component/ImageGenerator/ImageGenerator.jsx
+++ src/Component/ImageGenerator/ImageGenerator.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import './ImageGenerator.css'
+import default_image from '../Assets/default_image.svg'
 
 const ImageGenerator = () => {
   return (
-    <div>ImageGenerator</div>
+    <div className='ai-image-generator'>
+      
+      <div className="header">Ai Image <span>generator</span></div>
+      <div className='img-loading'>
+        <div className="image"><img src={default_image} alt="" /></div>
+      </div>
+    </div>
   )
 }
 
