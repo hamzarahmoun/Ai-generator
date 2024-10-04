2dbfea9250072604ef3b0eed6eb9cec8dc27de7b
hamzarahmoun <hamza.rahmoun456@gmail.com>
2024-10-04 12:17:21 +0100

    design structure and css done

--- src/Component/ImageGenerator/ImageGenerator.jsx
+++ src/Component/ImageGenerator/ImageGenerator.jsx
@@ -10,8 +10,12 @@ const ImageGenerator = () => {
       <div className='img-loading'>
         <div className="image"><img src={default_image} alt="" /></div>
       </div>
+      <div className="search-box">
+        <input type="text" className='search-input' placeholder='describe what you want to see'/>
+        <div className="generate-btn">Generate</div>
+      </div>
     </div>
   )
 }
 
-export default ImageGenerator
+export default ImageGenerator 
